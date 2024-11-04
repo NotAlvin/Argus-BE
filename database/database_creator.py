574e1ab807c36d51c48cba368b2debedee3ab521
alvin-leung <alvin.leung@juliusbaer.com>
2024-11-04 15:38:31 +0800

    Testing report generation function

--- database/database_creator.py
+++ database/database_creator.py
@@ -6,13 +6,14 @@ from datetime import datetime
 
 import pandas as pd
 import requests
+import ast
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from tqdm import tqdm
 
-from utils.base_templates import Insider, Company, NewsArticle
+from utils.base_templates import Insider, Company, NewsArticle, ArticleCollection
 
 '''
 Helper functions and global variables
@@ -31,6 +32,17 @@ def get_random_user_agent():
 
 def complete_href(href: str) -> str:
     return f'https://www.marketscreener.com{href}'
+
+def str_to_dict_expansion(dict_repr: any) -> dict:
+    dict_repr = str(dict_repr)
+    if dict_repr.strip() == '{}':
+        return {}
+    else:
+        try:
+            return ast.literal_eval(dict_repr.strip())
+        except Exception as e:
+            print(e)
+            return {}
 
 '''
 Scraping Insiders
@@ -586,3 +598,187 @@ Scraping News Articles
 
 This module provides functions to scrape News articles from the MarketScreener website.
 '''
+
+def parse_time_string(time_str: str) -> datetime:
+    """
+    Parses a time string into a datetime object with the current date or year.
+    
+    Args:
+        time_str (str): The time string to convert, e.g., '02:55am', 'Oct. 14', or '2024-10-20'.
+        
+    Returns:
+        datetime: A datetime object with the appropriate date and time.
+    """
+    formats = [
+        ('%I:%M%p', lambda t: datetime.combine(datetime.now().date(), t.time())),
+        ('%b. %d', lambda d: datetime(datetime.now().year, d.month, d.day)),
+        ('%Y-%m-%d', lambda d: d)
+    ]
+    
+    for fmt, constructor in formats:
+        try:
+            parsed_date = datetime.strptime(time_str, fmt)
+            return constructor(parsed_date)
+        except ValueError:
+            continue
+    
+    raise ValueError(f"Time string '{time_str}' does not match expected formats.")
+
+def fetch_html_content(url: str) -> str:
+    """
+    Fetches and returns the HTML content of a given URL.
+    
+    Args:
+        url (str): The URL to fetch.
+        
+    Returns:
+        str: The HTML content of the page.
+    """
+    try:
+        response = requests.get(url, headers=get_random_user_agent())
+        response.raise_for_status()
+        return urllib.parse.unquote(response.text)
+    except requests.RequestException as e:
+        raise RuntimeError(f"Failed to fetch URL {url}: {e}")
+
+def extract_article_text(soup: BeautifulSoup) -> str:
+    """
+    Extracts the article text from a BeautifulSoup object.
+    
+    Args:
+        soup (BeautifulSoup): The BeautifulSoup object containing the HTML content.
+        
+    Returns:
+        str: The extracted article text.
+    """
+    article_div = soup.find('div', class_='txt-s4 article-text')
+    if not article_div:
+        return 'Article text not found in the provided URL'
+    return article_div.get_text(separator='\n', strip=True)
+
+def extract_marketscreener_article(url: str) -> str:
+    """
+    Extracts the article text from a MarketScreener article URL.
+    
+    Args:
+        url (str): The URL of the MarketScreener article.
+        
+    Returns:
+        str: The extracted article text.
+    """
+    try:
+        html_content = fetch_html_content(url)
+        soup = BeautifulSoup(html_content, 'html.parser')
+        return extract_article_text(soup)
+    except Exception as e:
+        return f'Error with extracting article text: {e}'
+def get_article_from_link(article_link: str):
+    soup = fetch_html_content(article_link)
+    soup = BeautifulSoup(soup)
+    article_content = extract_article_text(soup)
+    article_headline = soup.find('title').get_text(separator='\n', strip=True)
+    # Extract the date and title attributes
+    span = soup.find('span', class_='js-date-relative')
+    publication_date = span.get('data-utc-date')
+
+    component_list = article_link.split('/')
+    company_link = '/' + '/'.join(component_list[3:6])
+    return {
+        'headline': article_headline,
+        'content': article_content,
+        'company_link': company_link,
+        'link': article_link,
+        'publication_date': publication_date
+    }
+
+def get_articles_from_marketscreener(url: str) -> ArticleCollection:
+    """
+    Retrieves articles from a MarketScreener page.
+    
+    Args:
+        url (str): The URL of the MarketScreener page.
+        
+    Returns:
+        ArticleCollection: A collection of articles extracted from the page.
+    """
+    try:
+        html_content = fetch_html_content(url)
+        soup = BeautifulSoup(html_content, 'html.parser')
+    except RuntimeError as e:
+        print(e)
+        return ArticleCollection()
+
+    articles_list = []  # Initialize a list to store NewsArticle objects
+    category = "MarketScreener "  # Assuming the source is MarketScreener
+    extraction_date = datetime.now()  # Current date and time for extraction
+
+    tables = soup.find_all('table')
+    for table in tables:
+        for row in table.find_all('tr'):
+            headline_tag = row.find('a', class_='c txt-s1 txt-overflow-2 link link--no-underline my-5 my-m-0')
+            if not headline_tag:
+                continue
+
+            headline = headline_tag.text.strip()
+            link = 'https://www.marketscreener.com' + headline_tag.get('href')
+            # Find the company link
+            company_tag = row.find('a', class_='link link--blue c-flex align-top')
+            company_name = company_tag.get('title') if company_tag else None
+            company_link = company_tag.get('href') if company_tag else None
+
+            source_tag = row.find('span', class_='c-block p-5 badge badge--small txt-s1')
+            source = source_tag.get('title') if source_tag else source
+            time_tag = row.find('span', class_='js-date-relative txt-muted h-100')
+            publication_date = parse_time_string(time_tag.text.strip()) if time_tag else None
+
+            article = NewsArticle(
+                headline=headline,
+                content=extract_marketscreener_article(link),
+                link=link,
+                company_name=company_name,
+                company_link=company_link,
+                source=source,
+                publication_date=publication_date
+            )
+            articles_list.append(article)  # Append each NewsArticle to the list
+
+    # Create and return an ArticleCollection object with all fields filled
+    return ArticleCollection(
+        extraction_date=extraction_date,
+        articles=articles_list
+    )
+
+def save_articles_to_csv(endpoint_list, base_url, csv_file_name):
+    """
+    Iterates through the endpoint list, retrieves article collections, and saves them to a CSV file.
+    
+    Args:
+        endpoint_list (list): List of endpoints to retrieve articles from.
+        base_url (str): The base URL for the MarketScreener news.
+        csv_file_name (str): The name of the CSV file to save the articles.
+    """
+    all_articles = []
+
+    for endpoint in endpoint_list:
+        url = f'{base_url}/{endpoint}/'
+        articles = get_articles_from_marketscreener(url)
+        
+        for article in articles.articles:
+            all_articles.append({
+                'category': endpoint,
+                'extraction_date': articles.extraction_date,
+                'headline': article.headline,
+                'content': article.content,
+                'company_name': article.company_name,
+                'company_link': article.company_link,
+                'link': article.link,
+                'source': article.source,
+                'publication_date': article.publication_date
+            })
+
+    # Convert the list of dictionaries to a DataFrame
+    df = pd.DataFrame(all_articles)
+
+    # Save the DataFrame to a CSV file
+    df.to_csv(csv_file_name, index=False)
+    print(f"Data saved to {csv_file_name}")
---
+++ reports/Norne_Securities_lowers_target_price_for_Belships_to_NOK_28_(30),_reiterates_Buy_-_BN_-_MarketScreener_2024-11-04T07:06:20+00:00.md
@@ -0,0 +1,18 @@
+# Norne Securities lowers target price for Belships to NOK 28 (30), reiterates Buy - BN - MarketScreener
+**Publication Date:** 2024-11-04T07:06:20+00:00
+**Category:** None
+**Source:** None
+
+## Article Content
+Norne Securities lowers target price for Belships to NOK 28 (30), reiterates Buy - BN
+
+## Company Information
+- **Name:** None
+- **ISIN:** NO0003094104
+- **Ticker:** BELCO
+- **Industry:** None
+- **Sector:** None
+- **Country:** None
+- **Profile:** Belships ASA is a Norway\-based shipping company and is a fully integrated shipowner and operator of dry bulk carriers. The Company provides shipping transportation services in a global market with customers and partners in all parts of the world. Commercial operations are provided by Lighthouse Navigation and the technical management and crewing by Belships Management, both subsidiaries of Belships ASA. The Companyâs activities are divided into two business segments: The Dry bulk segment and The Technical management segment. The Company operates worldwide.
+
+## Linked Insiders
